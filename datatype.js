fb7b473f16e046846993891d41dbc95e1ba0df93
i-OmSharma <sharmaom1201@gmail.com>
2024-03-02 19:00:39 +0530

    datatype js

---
+++ datatype.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version,there is no way for 'no strict'
+
+//alert( 1 + 1 )//we are using node js not browser
+
+/*Documentation :- ECMS(ECMAScript),MDN*/
+
+let name = "xyz"
+let age = "16"
+let isLoggedIn = false
+
+// number => 2^ 53 to 52
+// bigint => above number very big numbers
+// string => ""
+// boolean => T/F
+// null => standalone value/Empty(khaali h!)->type of = object
+// undefined => variable declared but value not given. -> typeof = undefined
+// symbol =>uniquness                            
+
+// object 
+// Function
+// Derived 
+
+console.log(typeof "xyz");
