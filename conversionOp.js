0e10201c946bdb58d92bc14c62ff77165a22c82e
i-OmSharma <sharmaom1201@gmail.com>
2024-03-03 11:19:08 +0530

    conversion operation js

---
+++ conversionOp.js
@@ -0,0 +1,36 @@
+//let score = 11 -> number
+//let score = "11" //->string
+//let score = "11abc"
+//let score = null
+//let score = undefined
+//let score = true
+//let score = "XYZ"
+
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//let valueInNumber = Number(score) //conversion 
+
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"11" => 11
+//"11abc" => NaN
+//true => 1; false=> 0
+
+//let isLoggedIn = 1
+//let isLoggedIn = "" -> false
+//let isLoggedIn = "abc" -> true
+
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "abc" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
