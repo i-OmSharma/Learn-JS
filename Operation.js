6035c8b6d3fba6d39038c5703cbe5dd6e5d5c6bb
i-OmSharma <sharmaom1201@gmail.com>
2024-03-04 19:31:38 +0530

    Operations js

---
+++ Operation.js
@@ -0,0 +1,36 @@
+let value = 3
+let negValue = -value //-assigned -ve value
+
+// console.log(negValue);
+
+// console.log(2+2); ->addition
+// console.log(2-2); ->substract
+// console.log(2*2); ->multiplication
+// console.log(2**2); ->power(2^2)
+// console.log(2/2); ->divide
+// console.log(2%2); ->remainder
+
+let str1 = "hi"
+let str2 = "ok"
+let str3 = str1+str2
+
+// console.log(str3);
+
+// console.log("1"+ 2);
+// console.log(1+"2");
+// console.log("1"+2+2); ->string at frist all will be converted into string
+// console.log(1+2+"2"); ->string at last number addition at first after that string addition
+
+//  console.log((1+2)*3/4); ->use parenthesis for multiple or complex operations
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2 -> assignig multiple variable same value,but wrong way
+
+let gameCounter = 100
+gameCounter++; //->postrfix
+// ++gameCounter; //->prefix
+console.log(gameCounter);
