509a46cb8bd6c8337206967a00df065223aa53e7
i-OmSharma <sharmaom1201@gmail.com>
2024-03-23 13:11:07 +0530

    De-Structure in JS

---
+++ Basics_2/de-structure.js
@@ -0,0 +1,21 @@
+const game = {
+    gameName: "GTA6",
+    price: "12000",
+    gameDevelper: "Rockstar"
+}
+
+// game.gameDevelper
+
+// const {gameDevelper} = game
+const {gameDevelper: Dev} = game
+console.log(Dev);
+
+/*****************react eample*******************/
+
+// const navbar = ({conpany}) => {
+
+// }
+
+// navbar(company = "Om")
+
+/***********************************************/
