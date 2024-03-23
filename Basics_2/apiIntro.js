d9c732454a028d4ddc7267cee42423e78d3f350b
i-OmSharma <sharmaom1201@gmail.com>
2024-03-23 13:12:21 +0530

    intro API

---
+++ Basics_2/apiIntro.js
@@ -0,0 +1,21 @@
+// JSON
+
+// {
+//        this is JSON,the object but without object name!!!
+// } 
+
+// keys here are also string and value is also string.
+
+// {
+//     "gameName": "GTA6",
+//     "price": "12000",
+//     "gameDevelper": "Rockstar"
+// }
+
+[
+    {},
+    {},
+    {},
+    {}
+]
+// JSON Formator:- tool for understanding json info n all.
