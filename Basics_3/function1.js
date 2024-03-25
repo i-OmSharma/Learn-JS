5af1dd490952d7661726bbcf18da08a172a5624b
i-OmSharma <sharmaom1201@gmail.com>
2024-03-25 11:41:02 +0530

    Function + Object JS

--- Basics_3/function1.js
+++ Basics_3/function1.js
@@ -38,6 +38,36 @@ function userLogin(userName){
 }
 
 // console.log(userLogin("Om"));
-console.log(userLogin()); // no value -> Undefined!
+// console.log(userLogin()); // no value -> Undefined!
 
+/******************************************************************************************/
 
+function calaculateCartPrice(val1, ...num1){ //"..." ->  this 3 dots are called 'RestOperator'&'Spreadoperator' it depends upon the situation where u use it like rest or spread.
+    return num1
+}
+
+// console.log(calaculateCartPrice(200, 400, 500));
+
+const user = {  //object
+    username: "Om",
+    price: 299
+}
+
+function handleObject(anyobject){  //function
+    console.log(`UserName is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "Mas",
+    price: 99
+})
+
+const newArr = [99,199,69]
+
+function returnSecondVal(getArr){
+    return getArr[1] 
+}
+
+// console.log(returnSecondVal(newArr));
+console.log(returnSecondVal(99,199,69));
