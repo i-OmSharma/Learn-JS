bc8eaab2c4e12b74647e2ac43c539a305eff23ba
i-OmSharma <sharmaom1201@gmail.com>
2024-04-12 21:16:31 +0530

    Switch JS

---
+++ ControlFlow/switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "feb"
+
+switch (month) {
+    case "jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+
+    default:
+        console.log("default code match");
+        break;
+}
