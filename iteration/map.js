e72a945c194461f5d5d52789de0ff19930c5e375
i-OmSharma <sharmaom1201@gmail.com>
2024-10-20 18:01:53 +0530

    map.js modified

--- iteration/map.js
+++ iteration/map.js
@@ -1,19 +1,18 @@
-const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 // console.log(myNums.map( (num) => num + 10 ));
 
 // const newNums = []
 // myNums.forEach( (nums) => {
 //     newNums.push( nums + 10 )
-    
-// } )
 
+// } )
 
 // Chaining:-
 
 const newNums = myNums
-                    .map( (num) => num * 10 )
-                    .map( (num) => num + 1 )
-                    .filter( (num) => num >= 15 )
+  .map((num) => num * 10)
+  .map((num) => num + 1) // this will get the updated value filtered  from the upper map function
+  .filter((num) => num >= 15);
 
 console.log(newNums);
